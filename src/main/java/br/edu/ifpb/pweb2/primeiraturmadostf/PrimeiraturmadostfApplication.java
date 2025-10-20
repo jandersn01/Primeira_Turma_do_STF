34a2547be0f0059bee0e31679d52523bb023b372
janderson01 <jandersonsantos030@gmail.com>
2025-10-20 11:57:05 -0300

    first commit

---
+++ src/main/java/br/edu/ifpb/pweb2/primeiraturmadostf/PrimeiraturmadostfApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.primeiraturmadostf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraturmadostfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraturmadostfApplication.class, args);
+	}
+
+}
