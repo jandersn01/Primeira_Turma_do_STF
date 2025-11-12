dfa74f9717d6a9c3589b199520bbd19c6fd11862
vtrnduda <vtrnduda@gmail.com>
2025-11-11 21:49:21 -0300

    feat: adiciona classes Professor, Aluno e Assunto para validar criação no banco. Necessário finalizar de acordo com as especificações do projeto

---
+++ src/main/java/br/edu/ifpb/pweb2/primeiraturmadostf/model/Aluno.java
@@ -0,0 +1,11 @@
+package br.edu.ifpb.pweb2.primeiraturmadostf.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Aluno {
+
+    @Id @GeneratedValue
+    private Long id;
+
+}
---
+++ src/main/java/br/edu/ifpb/pweb2/primeiraturmadostf/model/Assunto.java
@@ -0,0 +1,11 @@
+package br.edu.ifpb.pweb2.primeiraturmadostf.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Assunto {
+
+    @Id @GeneratedValue
+    private Long id;
+
+}
