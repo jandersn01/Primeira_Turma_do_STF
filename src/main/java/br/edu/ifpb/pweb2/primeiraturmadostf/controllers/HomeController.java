899e569d0f11b47eb0baadc2b6b6a0960d8c4e6e
janderson01 <jandersonsantos030@gmail.com>
2025-11-21 00:55:15 -0300

    Foi alterado a versão do SpringBoot de 3.5.6 > 3.3.5 pois, por alguma razão, a versão não estava sendo compatível com o thymeleaf.
    Foi comentado um atributo da classe Professor o qual se relaciona com
    reunião. Esse relacionamento não existe no diagrama e classes.
    
    Foi adicionado bootstrap por cdn.

--- src/main/java/br/edu/ifpb/pweb2/primeiraturmadostf/controllers/HomeController.java
+++ src/main/java/br/edu/ifpb/pweb2/primeiraturmadostf/controllers/HomeController.java
@@ -2,20 +2,25 @@ package br.edu.ifpb.pweb2.primeiraturmadostf.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.annotation.PostConstruct;
 
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("/")
-    public String redirecioanemtoRaiz(){
-        return "redirect:/home";
+    @GetMapping
+    public String exibirHome(){
+        return "auth/login";
     }
 
-    @GetMapping("/home")
-    public String exibirHome(){
-        return "layout/home";
-    }
+    @PostConstruct
+public void init() {
+    System.out.println("HomeController carregado!");
+}
+
 
 
 
