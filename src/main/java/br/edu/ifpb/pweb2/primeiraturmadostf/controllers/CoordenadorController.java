c8685947117e193d2f24649468d961235a472110
janderson01 <jandersonsantos030@gmail.com>
2025-12-03 02:45:10 -0300

    feat: adiciona lógica para que o coordenador distribua os processos

--- src/main/java/br/edu/ifpb/pweb2/primeiraturmadostf/controllers/CoordenadorController.java
+++ src/main/java/br/edu/ifpb/pweb2/primeiraturmadostf/controllers/CoordenadorController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.ifpb.pweb2.primeiraturmadostf.model.Aluno;
 import br.edu.ifpb.pweb2.primeiraturmadostf.model.Colegiado;
@@ -130,6 +132,29 @@ public class CoordenadorController {
         }
 
         return "coordenador/processo/list";
+    }
+
+    @PostMapping("/processo/distribuir")
+    public String distribuirProcesso(
+            @RequestParam("processoId") Long processoId,
+            @RequestParam("relatorId") Long relatorId,
+            @RequestParam(name = "colegiadoId", required = false) Long colegiadoId, // <---- matém o filtro!!!!!!!
+            RedirectAttributes attr) {
+        
+        try {
+            processoService.distribuirProcesso(processoId, relatorId);
+            attr.addFlashAttribute("mensagem", "Processo distribuído com sucesso!");
+        } catch (Exception e) {
+            attr.addFlashAttribute("mensagemErro", "Erro ao distribuir: " + e.getMessage());
+        }
+
+        // Redireciona para a lista. Se tivermos o ID do colegiado, mantemos o filtro.
+        String redirect = "redirect:/coordenador/processo/list";
+        if (colegiadoId != null) {
+            redirect += "?colegiadoId=" + colegiadoId;
+        }
+        
+        return redirect;
     }
 }
 
